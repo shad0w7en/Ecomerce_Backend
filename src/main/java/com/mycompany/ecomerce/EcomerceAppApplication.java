47513a60150cc9c93cdce65f16e108e16f856122
shad0w7en <ankuryadav93932@gmail.com>
2025-02-02 21:09:00 +0000

    Initial commit

---
+++ src/main/java/com/mycompany/ecomerce/EcomerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.ecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/ecomerce/EcomerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
